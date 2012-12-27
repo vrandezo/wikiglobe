3b07bf84deeb86123d7ffb1843a33f6c4aca63e1
vrandezo <denny.vrandecic@wikimedia.de>
2012-12-27 02:53:20 +0100

    eight languages

--- globe.js
+++ globe.js
@@ -64,7 +64,7 @@ DAT.Globe = function(container, colorFn) {
         'varying vec3 vNormal;',
         'void main() {',
           'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+          'gl_FragColor = vec4( 0.1, 0.2, 0.5, 0.7 ) * intensity;',
         '}'
       ].join('\n')
     }
